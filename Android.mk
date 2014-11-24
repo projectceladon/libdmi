a89532e13ecce76cf33cf83411c1d0a4b75f4584
Traian Schiau <traianx.schiau@intel.com>
2014-11-24 16:39:18 +0200

    intel_fw_props: Remove intel_fw_props
    The same functionality is now provided by intel_prop
    
    Change-Id: I2b49145cf3077285d7b9e8ee8cec57cc54fc6606
    Tracked-On: IMINAN-9503
    Signed-off-by: Traian Schiau <traianx.schiau@intel.com>
    Reviewed-on: 304543

--- Android.mk
+++ Android.mk
@@ -13,14 +13,3 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
 
 include $(BUILD_STATIC_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/sbin
-LOCAL_MODULE := intel_fw_props
-LOCAL_SRC_FILES:= \
-	intel_fw_props.c \
-
-LOCAL_STATIC_LIBRARIES := liblog libdmi libcutils
-
-include $(BUILD_EXECUTABLE)
-
